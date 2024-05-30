a987b57a91edb72c8bd603e985204b1c7b0b9612
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 20:49:21 +0700

    refactor: Improve accessibility by adding "break-words" class to CardCourses title

--- src/components/ui/CardCourses.jsx
+++ src/components/ui/CardCourses.jsx
@@ -4,7 +4,7 @@ export default function CardCourses() {
   return (
     <div className="flex flex-col gap-4">
       <img src={ImageCourse} alt="" />
-      <h1 className="text-xl font-medium text-secondary">
+      <h1 className="break-words text-xl font-medium text-secondary">
         hfcuerdgbufcbswbdehgsqvl
       </h1>
       <p className="break-words text-base text-black/50">
