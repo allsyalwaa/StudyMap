c82bb05c7743bc4ebbd0d565727eb194206a6fb7
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 20:49:33 +0700

    refactor: Update SecCourses grid layout for responsive design

--- src/components/section/SecCourses.jsx
+++ src/components/section/SecCourses.jsx
@@ -5,7 +5,7 @@ export default function SecCourses() {
     <section className="container my-12 py-10">
       <h2 className="text-3xl font-medium">Courses</h2>
 
-      <div className="mt-8 grid grid-cols-3 gap-6">
+      <div className="mt-8 grid grid-cols-2 gap-6 md:grid-cols-3">
         <CardCourses />
         <CardCourses />
         <CardCourses />
