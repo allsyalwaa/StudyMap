e38324c6d0aabd59962ec87658c446250c3f50f2
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 19:25:40 +0700

    refactor: Add ScoreQuiz component for displaying quiz score

---
+++ src/components/ui/ScoreQuiz.jsx
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+export default function ScoreQuiz() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+      <div className="w-full max-w-sm rounded-lg bg-white p-6 shadow-lg">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-center text-2xl font-bold">Your Score</h1>
+          <div className="mt-4 flex items-end justify-center gap-2">
+            <p className="text-center text-6xl font-bold ">100</p>
+            <p className="text-center text-4xl font-bold text-black/50">/100</p>
+          </div>
+          <div className="mx-auto mb-2 flex flex-row gap-4">
+            <Button variant="white-primary" className="mt-6">
+              Back
+            </Button>
+            <Button variant="primary" className="mt-6">
+              Continue
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
