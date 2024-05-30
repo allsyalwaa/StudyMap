bb627a7b38c87003b3064bc88442a915920235d4
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 11:03:19 +0700

    chore: Add Button component to project

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,16 @@
+export default function Button({ children, variant = "primary", className }) {
+  return (
+    <button
+      className={`
+        flex items-center justify-center gap-4 rounded-lg border-[1.5px] px-5 py-2 text-base font-medium
+        ${variant === "secondary" && "border-secondary bg-secondary text-sm text-white"}
+        ${variant === "primary" && "border-primary bg-primary text-lg text-white"}
+        ${variant === "white" && "border-secondary text-sm text-secondary"}
+        ${variant === "white-primary" && "border-primary/50 text-lg text-primary"}
+        ${className}
+    `}
+    >
+      {children}
+    </button>
+  );
+}
