53c49f6cefd7d89e42a101294ab39b4302f929cf
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 18:07:58 +0700

    refactor: Update SecVideoPlay component to center video play button

--- src/components/section/SecVideoPlay.jsx
+++ src/components/section/SecVideoPlay.jsx
@@ -6,7 +6,7 @@ export default function SecVideoPlay() {
       <div className="flex flex-col gap-6">
         <div className="relative">
           <img src={ImageVideoPlay} alt="" />
-          <div className="absolute left-80 top-44 text-white">
+          <div className="absolute inset-0 flex items-center justify-center text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="3em"
