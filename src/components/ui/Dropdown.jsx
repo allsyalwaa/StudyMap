c44d6001f476f3e165369573643722377f841637
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 15:35:22 +0700

    refactor: Update Dropdown component styling and add menu items

--- src/components/ui/Dropdown.jsx
+++ src/components/ui/Dropdown.jsx
@@ -8,7 +8,7 @@ export default function Dropdown({ name }) {
     <>
       <button
         onClick={() => setShowDropdown(!showDropdown)}
-        className="relative flex items-center justify-between gap-3 rounded-lg px-8 py-2 hover:bg-purple-800 hover:text-white"
+        className="relative flex items-center justify-between gap-3 rounded-lg px-8 py-2 hover:bg-secondary hover:text-white"
       >
         {name}
         <svg
@@ -16,7 +16,7 @@ export default function Dropdown({ name }) {
           width="1em"
           height="1em"
           viewBox="0 0 512 512"
-          className={`mt-1 rotate-180 transform ${showDropdown ? "rotate-0 transition-all duration-300" : "rotate-180 transition-all duration-300"}`}
+          className={`mt-1 transform ${showDropdown ? "rotate-0 transition-all duration-300" : "rotate-180 transition-all duration-300"}`}
         >
           <path
             fill="none"
@@ -27,45 +27,82 @@ export default function Dropdown({ name }) {
             d="m112 328l144-144l144 144"
           />
         </svg>
-        <div
-          className={`absolute -bottom-2 size-10 translate-y-full rotate-45 bg-gray-200 ${
-            showDropdown ? "block" : "hidden"
-          }`}
-        ></div>
       </button>
+
+      {showDropdown && (
+        <div
+          className="fixed bottom-0 left-0 right-0 top-40 z-40 bg-black opacity-50"
+          onClick={() => setShowDropdown(false)}
+        ></div>
+      )}
 
       {/* menus dropdown */}
       <div
-        className={`absolute left-0 top-20 w-screen bg-gray-200 ${
+        className={`absolute left-0 top-[77px] z-50 w-screen bg-white ${
           showDropdown ? "block" : "hidden"
         }`}
       >
-        <ul className="container grid grid-cols-3">
+        <ul className="container grid grid-cols-3 text-base">
           <li>
             <NavLink to="/">
-              <button className="w-full px-8 py-2 hover:bg-purple-800 hover:text-white">
-                Home
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
               </button>
             </NavLink>
           </li>
           <li>
             <NavLink to="/">
-              <button className="w-full px-8 py-2 hover:bg-purple-800 hover:text-white">
-                Home
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
               </button>
             </NavLink>
           </li>
           <li>
             <NavLink to="/">
-              <button className="w-full px-8 py-2 hover:bg-purple-800 hover:text-white">
-                Home
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
               </button>
             </NavLink>
           </li>
           <li>
             <NavLink to="/">
-              <button className="w-full px-8 py-2 hover:bg-purple-800 hover:text-white">
-                Home
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
+              </button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
+              </button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
+              </button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
+              </button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
+              </button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              <button className="w-full px-8 py-2 text-secondary hover:bg-secondary hover:text-white">
+                Front-End Developer
               </button>
             </NavLink>
           </li>
