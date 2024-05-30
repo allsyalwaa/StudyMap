d425c5d9e6275f116d8d4e8f7ff446b811b3cbfb
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 20:48:14 +0700

    refactor: Update SecHero component layout for responsive design

--- src/components/section/SecHero.jsx
+++ src/components/section/SecHero.jsx
@@ -17,9 +17,11 @@ export default function SecHero() {
 
   return (
     <section className="container my-12">
-      <div className="grid grid-cols-5 items-center justify-center gap-36">
-        <div className="col-span-3">
-          <h1 className="text-6xl font-bold ">Learn Anywhere, Anytime!</h1>
+      <div className="grid items-center justify-center gap-12 md:grid-cols-5 md:gap-36">
+        <div className="text-center md:col-span-3 md:text-start">
+          <h1 className="text-4xl font-bold md:text-6xl ">
+            Learn Anywhere, Anytime!
+          </h1>
           <p className="mt-6 text-lg">
             With access to our online courses, you can learn anytime and
             anywhere. This flexibility allows you to tailor your learning to
@@ -32,7 +34,9 @@ export default function SecHero() {
             </Button>
           </button>
         </div>
-        <img className="col-span-2 w-full" src={ImageHero} alt="" />
+        <div className="grid justify-center md:col-span-2 md:w-full md:justify-end">
+          <img className="w-72" src={ImageHero} alt="" />
+        </div>
       </div>
 
       {isSignUpPopupOpen && <SignUp onClose={handleCloseSignUpPopup} />}
