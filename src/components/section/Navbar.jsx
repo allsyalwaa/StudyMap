e7d468aa689e3eda521296e0616f6563a0b1c5bd
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 11:01:47 +0700

    Update styling Navbar component

--- src/components/section/Navbar.jsx
+++ src/components/section/Navbar.jsx
@@ -1,48 +1,35 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Dropdown from "../ui/Dropdown";
 
-export default function Navbar() {
-  const [showDropdown, setShowDropdown] = useState(false);
+import Logo from "../../assets/logo.svg";
+import Button from "../ui/Button";
 
+export default function Navbar() {
   return (
     <nav>
-      <div className=" container grid h-20 grid-cols-3 items-center">
+      <div className=" container fixed grid h-[72px] grid-cols-3 items-center bg-white">
         {/* logo */}
         <NavLink to="/" className={"text-xl font-bold"}>
-          Logo
+          <img className="h-16" src={Logo} alt="" />
         </NavLink>
 
         {/* menus */}
         <div className="flex justify-center">
-          <ul className="flex justify-center rounded-lg bg-purple-200">
-            <li>
-              <Dropdown name="testing" />
-            </li>
-            <li>
-              <button
-                onClick={() => setShowDropdown(!showDropdown)}
-                className="relative flex justify-center rounded-lg px-8 py-2 hover:bg-purple-800 hover:text-white"
-              >
-                Dropdown
-                <div
-                  className={`absolute -bottom-2 size-10 translate-y-full rotate-45 bg-gray-200 ${
-                    showDropdown ? "block" : "hidden"
-                  }`}
-                ></div>
-              </button>
+          <ul className="flex items-center justify-center rounded-lg bg-primary/5">
+            <li className="flex items-center gap-1 text-base font-medium text-secondary">
+              <Dropdown name="Course" />
             </li>
             <li>
               <NavLink to="/">
-                <button className="rounded-lg px-8 py-2 hover:bg-purple-800 hover:text-white">
-                  Home
+                <button className="rounded-lg px-8 py-2 text-base font-medium text-secondary hover:bg-purple-800 hover:text-white">
+                  About
                 </button>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about">
-                <button className="rounded-lg px-8 py-2 hover:bg-purple-800 hover:text-white">
-                  About
+              <NavLink to="/">
+                <button className="rounded-lg px-8 py-2 text-base font-medium text-secondary hover:bg-purple-800 hover:text-white">
+                  Contact
                 </button>
               </NavLink>
             </li>
@@ -53,52 +40,14 @@ export default function Navbar() {
         <ul className="ml-auto flex ">
           <li>
             <NavLink to="/">
-              <button className="rounded-lg px-8 py-2">Login</button>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/">
-              <button className="rounded-lg bg-purple-600 px-8 py-2 text-white">
-                Register
-              </button>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-
-      {/* menus dropdown */}
-      <div
-        className={`absolute w-screen bg-gray-200 ${
-          showDropdown ? "block" : "hidden"
-        }`}
-      >
-        <ul className="container grid grid-cols-3">
-          <li>
-            <NavLink to="/">
-              <button className="w-full px-8 py-2 hover:bg-purple-800 hover:text-white">
-                Home
+              <button className="rounded-lg px-8 py-2 text-base font-medium text-secondary">
+                Log In
               </button>
             </NavLink>
           </li>
           <li>
             <NavLink to="/">
-              <button className="w-full px-8 py-2 hover:bg-purple-800 hover:text-white">
-                Home
-              </button>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/">
-              <button className="w-full px-8 py-2 hover:bg-purple-800 hover:text-white">
-                Home
-              </button>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/">
-              <button className="w-full px-8 py-2 hover:bg-purple-800 hover:text-white">
-                Home
-              </button>
+              <Button variant="primary">Sign Up</Button>
             </NavLink>
           </li>
         </ul>
