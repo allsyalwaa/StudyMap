27fd35a502357c16a0ded58aa9871c52a143316b
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 18:06:47 +0700

    refactor: Update User component to include user menu with logout functionality

--- src/components/ui/User.jsx
+++ src/components/ui/User.jsx
@@ -1,10 +1,84 @@
+import { useState } from "react";
 import ImageUser from "../../assets/illustration-user.svg";
+import { NavLink } from "react-router-dom";
 
 export default function User() {
+  const [isMenuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!isMenuVisible);
+  };
+
   return (
-    <button className="flex items-center justify-end gap-8">
-      <h1 className="text-lg font-medium text-secondary">Username</h1>
-      <img className="h-10" src={ImageUser} alt="" />
-    </button>
+    <div className="relative flex items-center justify-end">
+      <button onClick={toggleMenu} className="flex items-center gap-8">
+        <h1 className="text-lg font-medium text-secondary">Username</h1>
+        <img className="h-10" src={ImageUser} alt="" />
+      </button>
+
+      {/* menu logout */}
+      {isMenuVisible && (
+        <div className="absolute right-0 top-14 bg-white p-6">
+          <div className="grid grid-cols-3 items-center gap-4 px-2">
+            <img className="h-16" src={ImageUser} alt="" />
+            <div className="col-span-2 flex flex-col">
+              <h1 className="text-xl font-semibold">Username</h1>
+              <p className="text-base text-black/50">emailemail@gmail.com</p>
+            </div>
+          </div>
+          <div className="mt-4 w-full border-b-[1px] border-black/50"></div>
+          <div className="mt-4 flex flex-col gap-4">
+            <NavLink to="/">
+              <div className="flex items-center gap-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.5em"
+                  height="1.5em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M5.85 17.1q1.275-.975 2.85-1.537T12 15t3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4T6.337 6.338T4 12q0 1.475.488 2.775T5.85 17.1M12 13q-1.475 0-2.488-1.012T8.5 9.5t1.013-2.488T12 6t2.488 1.013T15.5 9.5t-1.012 2.488T12 13m0 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q1.325 0 2.5-.387t2.15-1.113q-.975-.725-2.15-1.112T12 17t-2.5.388T7.35 18.5q.975.725 2.15 1.113T12 20m0-9q.65 0 1.075-.425T13.5 9.5t-.425-1.075T12 8t-1.075.425T10.5 9.5t.425 1.075T12 11m0 7.5"
+                  />
+                </svg>
+                <h1 className="text-base font-medium">Account</h1>
+              </div>
+            </NavLink>
+            <NavLink to="/">
+              <div className="flex items-center gap-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.5em"
+                  height="1.5em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M19.9 12.66a1 1 0 0 1 0-1.32l1.28-1.44a1 1 0 0 0 .12-1.17l-2-3.46a1 1 0 0 0-1.07-.48l-1.88.38a1 1 0 0 1-1.15-.66l-.61-1.83a1 1 0 0 0-.95-.68h-4a1 1 0 0 0-1 .68l-.56 1.83a1 1 0 0 1-1.15.66L5 4.79a1 1 0 0 0-1 .48L2 8.73a1 1 0 0 0 .1 1.17l1.27 1.44a1 1 0 0 1 0 1.32L2.1 14.1a1 1 0 0 0-.1 1.17l2 3.46a1 1 0 0 0 1.07.48l1.88-.38a1 1 0 0 1 1.15.66l.61 1.83a1 1 0 0 0 1 .68h4a1 1 0 0 0 .95-.68l.61-1.83a1 1 0 0 1 1.15-.66l1.88.38a1 1 0 0 0 1.07-.48l2-3.46a1 1 0 0 0-.12-1.17ZM18.41 14l.8.9l-1.28 2.22l-1.18-.24a3 3 0 0 0-3.45 2L12.92 20h-2.56L10 18.86a3 3 0 0 0-3.45-2l-1.18.24l-1.3-2.21l.8-.9a3 3 0 0 0 0-4l-.8-.9l1.28-2.2l1.18.24a3 3 0 0 0 3.45-2L10.36 4h2.56l.38 1.14a3 3 0 0 0 3.45 2l1.18-.24l1.28 2.22l-.8.9a3 3 0 0 0 0 3.98m-6.77-6a4 4 0 1 0 4 4a4 4 0 0 0-4-4m0 6a2 2 0 1 1 2-2a2 2 0 0 1-2 2"
+                  />
+                </svg>
+                <h1 className="text-base font-medium">Setting</h1>
+              </div>
+            </NavLink>
+            <NavLink to="/">
+              <div className="flex items-center gap-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.5em"
+                  height="1.5em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z"
+                  />
+                </svg>
+                <h1 className="text-base font-medium">Log Out</h1>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
