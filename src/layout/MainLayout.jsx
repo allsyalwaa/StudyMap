ee13e9b082992894b2254fd98fc8e38acac0adb7
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 11:06:48 +0700

    chore: Add footer, about, courses, and hero sections to home page layout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,13 @@
+import Footer from "../components/section/Footer";
 import Navbar from "../components/section/Navbar";
 
-export default function MainLayout() {
+// eslint-disable-next-line react/prop-types
+export default function MainLayout({ children }) {
   return (
-    <>
+    <div>
       <Navbar />
-    </>
+      <main className="pt-16">{children}</main>
+      <Footer />
+    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
+import SecAbout from "../components/section/SecAbout";
+import SecCourses from "../components/section/SecCourses";
+import SecHero from "../components/section/SecHero";
 import MainLayout from "../layout/MainLayout";
 
 export default function Home() {
-  return <MainLayout></MainLayout>;
+  return (
+    <MainLayout>
+      <SecHero />
+      <SecCourses />
+      <SecAbout />
+    </MainLayout>
+  );
 }
