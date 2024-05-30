09b2ee53b6a214c1a9cdeeaea054a0efe6984161
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-30 14:26:18 +0700

    refactor: Update LogIn component to include onClose prop

--- src/components/ui/LogIn.jsx
+++ src/components/ui/LogIn.jsx
@@ -1,64 +1,78 @@
+/* eslint-disable react/prop-types */
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logo.svg";
 import Button from "./Button";
 
-export default function LogIn() {
+import { useState } from "react";
+
+export default function LogIn({ onClose }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const closePopup = () => {
+    setIsOpen(false);
+    onClose();
+  };
+
   return (
     <>
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-        <div className="w-full max-w-2xl rounded-3xl bg-white p-10 shadow-lg">
-          <div className="mb-4 flex justify-end">
-            <button className="text-primary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1.3em"
-                height="1.3em"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="m289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 0 0 0-34 34l95 95l-95 95a24 24 0 1 0 34 34l95-95l95 95a24 24 0 0 0 34-34Z"
-                />
-              </svg>
-            </button>
-          </div>
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+          <div className="w-full max-w-2xl rounded-3xl bg-white p-10 shadow-lg">
+            <div className="mb-4 flex justify-end">
+              <button onClick={closePopup} className="text-primary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.3em"
+                  height="1.3em"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="m289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 0 0 0-34 34l95 95l-95 95a24 24 0 1 0 34 34l95-95l95 95a24 24 0 0 0 34-34Z"
+                  />
+                </svg>
+              </button>
+            </div>
 
-          <div className="flex items-center justify-center">
-            <h1 className="mb-3 text-4xl font-bold">Log in to</h1>
-            <img src={Logo} alt="" />
-          </div>
+            <div className="flex items-center justify-center">
+              <h1 className="mb-3 text-4xl font-bold">Log in to</h1>
+              <img src={Logo} alt="" />
+            </div>
 
-          <div className="mt-2 flex items-center justify-center gap-1">
-            <p className="text-base  text-black">Don&apos;t have an account?</p>
-            <NavLink to="/sign-up" className="text-base underline">
-              Sign Up
-            </NavLink>
-          </div>
-
-          <form className="mt-6 flex flex-col" action="">
-            <input
-              className="mt-4 rounded-lg border-[1.5px] border-black/50 px-5 py-3 placeholder-black/50"
-              type="email"
-              placeholder="Email"
-            />
-            <input
-              className="mt-4  rounded-lg border-[1.5px] border-black/50 px-5 py-3 placeholder-black/50"
-              type="password"
-              placeholder="Password"
-            />
-
-            <div className="mt-2 flex justify-end">
-              <NavLink to="/" className="text-sm underline">
-                Forgot password?
+            <div className="mt-2 flex items-center justify-center gap-1">
+              <p className="text-base  text-black">
+                Don&apos;t have an account?
+              </p>
+              <NavLink to="/sign-up" className="text-base underline">
+                Sign Up
               </NavLink>
             </div>
 
-            <Button variant="primary" className={"mt-8 px-5 py-3"}>
-              Log In
-            </Button>
-          </form>
+            <form className="mt-6 flex flex-col" action="">
+              <input
+                className="mt-4 rounded-lg border-[1.5px] border-black/50 px-5 py-3 placeholder-black/50"
+                type="email"
+                placeholder="Email"
+              />
+              <input
+                className="mt-4  rounded-lg border-[1.5px] border-black/50 px-5 py-3 placeholder-black/50"
+                type="password"
+                placeholder="Password"
+              />
+
+              <div className="mt-2 flex justify-end">
+                <NavLink to="/" className="text-sm underline">
+                  Forgot password?
+                </NavLink>
+              </div>
+
+              <Button variant="primary" className={"mt-8 px-5 py-3"}>
+                Log In
+              </Button>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
