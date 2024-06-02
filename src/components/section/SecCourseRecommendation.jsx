1af71f4fb64c8fdadb5f21b42c98a62400186557
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-02 17:58:18 +0700

    refactor: Update SecCourseRecommendation component to display more course recommendations

--- src/components/section/SecCourseRecommendation.jsx
+++ src/components/section/SecCourseRecommendation.jsx
@@ -2,9 +2,14 @@ import CourseRecommendation from "../ui/CourseRecommendation";
 
 export default function SecCourseRecommendation() {
   return (
-    <section>
+    <section className="top-20">
       <div className="flex flex-col gap-6">
         <h2 className="text-xl font-medium">Course Recommendation</h2>
+        <CourseRecommendation />
+        <CourseRecommendation />
+        <CourseRecommendation />
+        <CourseRecommendation />
+        <CourseRecommendation />
         <CourseRecommendation />
         <CourseRecommendation />
         <CourseRecommendation />
