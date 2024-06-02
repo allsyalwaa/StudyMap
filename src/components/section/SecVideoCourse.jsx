90d9648c57c25c690128335cae018e4782d3bdcb
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-02 17:58:36 +0700

    refactor: Update SecVideoCourse component to improve mobile responsiveness

--- src/components/section/SecVideoCourse.jsx
+++ src/components/section/SecVideoCourse.jsx
@@ -4,8 +4,8 @@ export default function SecVideoCourse() {
   return (
     <section>
       <div className="flex flex-col gap-6">
-        <h2 className="text-xl font-medium">Front-End Developer</h2>
-        <div className="border-b-[1px] border-black/50"></div>
+        <h2 className="text-xl font-medium md:hidden">Playlist</h2>
+        <div className="border-b-[1px] border-black/50 md:hidden"></div>
         <CardVideoCourse />
         <CardVideoCourse />
         <CardVideoCourse />
