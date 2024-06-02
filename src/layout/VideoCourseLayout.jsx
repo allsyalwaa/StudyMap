9731bf5f017794ce070f2527f8fe2d993162e497
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-02 17:58:43 +0700

    refactor: Update VideoCourseLayout to improve mobile responsiveness and add course title section

--- src/layout/VideoCourseLayout.jsx
+++ src/layout/VideoCourseLayout.jsx
@@ -4,8 +4,12 @@ import SecVideoPlay from "../components/section/SecVideoPlay";
 export default function VideoCourseLayout() {
   return (
     <div className="container my-12">
-      <div className="grid grid-cols-3 gap-12">
-        <SecVideoCourse />
+      <div className="grid gap-12 md:grid-cols-3">
+        <div className="flex flex-col gap-6">
+          <h2 className="text-xl font-medium">Front-End Developer</h2>
+          <div className="border-b-[1px] border-black/50"></div>
+          <SecVideoCourse />
+        </div>
         <SecVideoPlay />
       </div>
     </div>
