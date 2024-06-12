05c6c44a685895022c629dc784663fb585b5051b
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 23:03:29 +0700

    refactor: Update CardVideoCourse component to include NavLink for image and title

--- src/components/ui/CardVideoCourse.jsx
+++ src/components/ui/CardVideoCourse.jsx
@@ -1,13 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
-export default function CardVideoCourse({ image, title }) {
+export default function CardVideoCourse({ image, title, to }) {
   return (
-    <div className=" flex items-center gap-6">
+    <NavLink to={to} className=" flex items-center gap-6">
       <img className="h-20" src={image} alt="" />
       <div className="flex w-1/3 flex-col">
         <h1 className="line-clamp-2 text-ellipsis break-words text-lg font-medium text-secondary">
           {title}
         </h1>
       </div>
-    </div>
+    </NavLink>
   );
 }
