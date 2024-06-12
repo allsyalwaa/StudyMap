5632aad479de17d2f0be8a500cb72c79d3500880
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 22:19:46 +0700

    refactor: Update SecVideoPlay component to use updated video play icon

--- src/components/section/SecVideoPlay.jsx
+++ src/components/section/SecVideoPlay.jsx
@@ -1,4 +1,4 @@
-import ImageVideoPlay from "../../assets/illustration-video-play.svg";
+import ImageVideoPlay from "../../assets/video-play.svg";
 
 export default function SecVideoPlay() {
   return (
@@ -20,9 +20,43 @@ export default function SecVideoPlay() {
             </svg>
           </div>
         </div>
-        <h1 className="text-2xl font-semibold">Judul video uhfuirehuycfdeh4</h1>
-        <div className="h-64 border-[1px] border-black/50 p-2">
-          <p>Description</p>
+        <h1 className="text-2xl font-semibold">
+          Frontend | Day 1 | Basics of HTML
+        </h1>
+        <div className="h-64 overflow-y-auto border-[1px] border-black/50 p-2 text-black/80">
+          <p>Credit by Devsnests</p>
+          <p className="mt-4">
+            Resources and assignments given on the website check it out!
+            https://devsnest.in 3 steps to join the course: 1. Sign up on
+            https://devsnest.in/?ref=yt and apply for the course. 2. Create your
+            discord account and join our 20K member strong tech community
+            https://devsnest.in/discord 3. Join our WhatsApp group for updates
+            and notifications https://devsnest.in/whatsapp How to join group:
+            https://water-pin-778.notion.site/Gro... Gaurav:
+            https://linktr.ee/gaurav.sen Kshitiz: / kshitizmi. . /
+            miglanikshitiz Vedansh: / vedanshgarg / vedansh_garg Timecodes 0:00
+            - Intro 1:41 - Instructor and Bootcamp Intro 3:50 - Introduction to
+            Web-Development 6:45 - Introduction to Frontend 8:55 - Basic
+            Requirements for Session 10:25 - Creating the first HTML Document
+            12:33 - Basic Tags to define HTML Document 14:47 - head tag in html
+            19:33 - body tag in html 22:00 - Importance of viewport 27:37 -
+            Anatomy of HTML Element 29:33 - Understanding heading Tags 32:46 -
+            Commenting in HTML 38:08 - paragraph tag in HTML 45:43 - lists in
+            HTML 50:15 - nesting of tags in HTML 54:00 - anchor tag in HTML
+            1:01:40 - image tag in HTML 1:14:37 - dividing the pages into
+            sections using rule tag 1:15:57 - Session Break 1:25:00 -HomeWork
+            for the session Devsnest Education which makes you financially
+            independent New batch launching Devsnest provides a 6+ month
+            mentorship program. A few things we offer to students : Six months
+            course covering everything they would need to become a rockstar
+            developer from Full-stack(Both backend and frontend) to Data
+            structures A platform that connects them with a super pumped up
+            crowd to learn with and collaborate in projects Mentorship from the
+            tech industry's top tech professionals from companies like Amazon,
+            Microsoft, Google, etc., all of them working to help the students to
+            reach their maximum potential. JOBS. We bring job opportunities,
+            freelancing projects, and internships for the students
+          </p>
         </div>
       </div>
     </div>
