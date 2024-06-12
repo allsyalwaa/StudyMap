2cfa2161d1a4783e8d026703bf24fab63ddc5fa5
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 21:58:05 +0700

    refactor: Update SecCourseRecommendation component to include "Coming Soon" links for each course

--- src/components/section/SecCourseRecommendation.jsx
+++ src/components/section/SecCourseRecommendation.jsx
@@ -15,6 +15,7 @@ export default function SecCourseRecommendation() {
           description={
             "Back end development covers server-side programming, databases, and APIs. Learn languages like Python, Java, or Node.js, along with tools for database management and server configuration. The course emphasizes performance, security, and scalability, offering hands-on projects and comprehensive learning."
           }
+          to={"/coming-soon"}
         />
         <CourseRecommendation
           image={fullstack1}
@@ -22,6 +23,7 @@ export default function SecCourseRecommendation() {
           description={
             "Full stack development covers both front end and back end technologies. Learn HTML, CSS, JavaScript, as well as server-side languages like Python or Node.js, and databases. The course emphasizes building complete applications, focusing on performance, security, and scalability, with hands-on projects for a comprehensive learning experience."
           }
+          to={"/coming-soon"}
         />
         <CourseRecommendation
           image={uiux1}
@@ -29,6 +31,7 @@ export default function SecCourseRecommendation() {
           description={
             "UI/UX design covers user interface and user experience principles. Learn design tools like Sketch or Figma, prototyping, user research, and usability testing. The course emphasizes creating intuitive, user-friendly designs, focusing on aesthetics, functionality, and user satisfaction, with hands-on projects for a comprehensive learning experience."
           }
+          to={"/coming-soon"}
         />
       </div>
     </section>
