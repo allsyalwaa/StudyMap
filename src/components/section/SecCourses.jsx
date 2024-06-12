310dcf7e13b6bcbee54ff7a3522749bf62e76a07
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 12:43:57 +0700

    refactor: Update SecCourses component to use SVG images for course cards

--- src/components/section/SecCourses.jsx
+++ src/components/section/SecCourses.jsx
@@ -1,4 +1,10 @@
 import CardCourses from "../ui/CardCourses";
+import frontend from "../../assets/frontend.svg";
+import backend from "../../assets/backend.svg";
+import fullstack from "../../assets/fullstack.svg";
+import uiux from "../../assets/uiux.svg";
+import api from "../../assets/api.svg";
+import websecurity from "../../assets/websecurity.svg";
 
 export default function SecCourses() {
   return (
@@ -6,12 +12,12 @@ export default function SecCourses() {
       <h2 className="text-3xl font-medium">Courses</h2>
 
       <div className="mt-8 grid grid-cols-2 gap-6 md:grid-cols-3">
-        <CardCourses />
-        <CardCourses />
-        <CardCourses />
-        <CardCourses />
-        <CardCourses />
-        <CardCourses />
+        <CardCourses image={frontend} />
+        <CardCourses image={backend} />
+        <CardCourses image={fullstack} />
+        <CardCourses image={uiux} />
+        <CardCourses image={api} />
+        <CardCourses image={websecurity} />
       </div>
     </section>
   );
