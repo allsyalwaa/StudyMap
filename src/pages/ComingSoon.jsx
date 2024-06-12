c6f077efd4551ebceb297ed39c2d7859b7b31928
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 18:49:47 +0700

    feat: Create ComingSoon page with MainLayout and SecComingSoon components

---
+++ src/pages/ComingSoon.jsx
@@ -0,0 +1,10 @@
+import MainLayout from "../layout/MainLayout";
+import SecComingSoon from "../components/section/SecComingSoon";
+
+export default function ComingSoon() {
+  return (
+    <MainLayout>
+      <SecComingSoon />
+    </MainLayout>
+  );
+}
