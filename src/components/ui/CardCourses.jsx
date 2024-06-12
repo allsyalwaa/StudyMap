f96a7ef1e5a3098d57de11698da1b950829d95d4
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 21:37:27 +0700

    refactor: Update CardCourses component to include NavLink for course navigation

--- src/components/ui/CardCourses.jsx
+++ src/components/ui/CardCourses.jsx
@@ -1,7 +1,9 @@
+import { NavLink } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
-export default function CardCourses({ image, title, description }) {
+export default function CardCourses({ image, title, description, to }) {
   return (
-    <div className="flex flex-col gap-4">
+    <NavLink to={to} className="flex flex-col gap-4">
       <img src={image} alt="" />
       <h1 className="break-words text-xl font-medium text-secondary">
         {title}
@@ -9,6 +11,6 @@ export default function CardCourses({ image, title, description }) {
       <p className="line-clamp-2 text-ellipsis break-words text-base text-black/50">
         {description}
       </p>
-    </div>
+    </NavLink>
   );
 }
