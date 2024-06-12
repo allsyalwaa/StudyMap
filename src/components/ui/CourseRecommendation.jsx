3a2914526374d1a4bb15d755e71af579be6503c1
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 21:58:18 +0700

    refactor: Update CourseRecommendation component to include "to" prop for course navigation

--- src/components/ui/CourseRecommendation.jsx
+++ src/components/ui/CourseRecommendation.jsx
@@ -1,13 +1,19 @@
 /* eslint-disable react/prop-types */
 import CardCourseRecommendation from "./CardCourseRecommendation";
 
-export default function CourseRecommendation({ image, title, description }) {
+export default function CourseRecommendation({
+  image,
+  title,
+  description,
+  to,
+}) {
   return (
     <div>
       <CardCourseRecommendation
         image={image}
         title={title}
         description={description}
+        to={to}
       />
     </div>
   );
