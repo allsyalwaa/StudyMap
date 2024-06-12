bf961ba64eef40a4b34dcdb8e3039f6cea947509
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 12:58:04 +0700

    refactor: Update CardCourses component to include title and description props

--- src/components/ui/CardCourses.jsx
+++ src/components/ui/CardCourses.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
-export default function CardCourses({ image }) {
+export default function CardCourses({ image, title, description }) {
   return (
     <div className="flex flex-col gap-4">
       <img src={image} alt="" />
       <h1 className="break-words text-xl font-medium text-secondary">
-        hfcuerdgbufcbswbdehgsqvl
+        {title}
       </h1>
-      <p className="break-words text-base text-black/50">
-        hfcuerdgbufcbswbdehgshqvlhfcuerdgbufcbswbdehgshqvlhfcuerdgbufcbswbdehsed...
+      <p className="line-clamp-2 text-ellipsis break-words text-base text-black/50">
+        {description}
       </p>
     </div>
   );
