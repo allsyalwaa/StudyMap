9ef7dc648b2ce4af7bbc1d0343f92d238ad94ebc
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 22:24:17 +0700

    refactor: Update SecQuiz component to include quiz questions with options and labels

--- src/components/section/SecQuiz.jsx
+++ src/components/section/SecQuiz.jsx
@@ -18,41 +18,57 @@ export default function SecQuiz() {
       <div className="flex flex-col justify-center gap-6">
         <h1 className="mt-6 text-center text-xl font-medium">Quiz 1</h1>
         <div className="flex flex-col gap-3 border-[1.5px] p-4">
-          <p>Question 1</p>
+          <p>
+            1. Apa fungsi utama dari Virtual DOM dalam framework seperti React?
+          </p>
           <div className="flex items-center gap-2">
             <input type="radio" name="question1" id="option1" />
-            <label htmlFor="option1">Option 1</label>
+            <label htmlFor="option1">Mengatur state aplikasi</label>
           </div>
           <div className="flex items-center gap-2">
             <input type="radio" name="question1" id="option2" />
-            <label htmlFor="option2">Option 2</label>
+            <label htmlFor="option2">
+              Mengoptimalkan rendering UI dengan meminimalkan manipulasi
+              langsung pada DOM nyata
+            </label>
           </div>
           <div className="flex items-center gap-2">
             <input type="radio" name="question1" id="option3" />
-            <label htmlFor="option3">Option 3</label>
+            <label htmlFor="option3">
+              Menyediakan penyimpanan data sementara di sisi klien
+            </label>
           </div>
           <div className="flex items-center gap-2">
             <input type="radio" name="question1" id="option4" />
-            <label htmlFor="option4">Option 4</label>
+            <label htmlFor="option4">Mengelola routing dalam aplikasi</label>
           </div>
         </div>
         <div className="flex flex-col gap-3 border-[1.5px] p-4">
-          <p>Question 2</p>
+          <p>
+            2. Manakah dari berikut ini yang merupakan keuntungan utama
+            menggunakan TypeScript dalam pengembangan front-end?
+          </p>
           <div className="flex items-center gap-2">
             <input type="radio" name="question2" id="option1" />
-            <label htmlFor="option1">Option 1</label>
+            <label htmlFor="option1">
+              Kemampuan untuk menulis kode dengan performa lebih tinggi
+            </label>
           </div>
           <div className="flex items-center gap-2">
             <input type="radio" name="question2" id="option2" />
-            <label htmlFor="option2">Option 2</label>
+            <label htmlFor="option2">
+              Dukungan untuk pemrograman berbasis objek
+            </label>
           </div>
           <div className="flex items-center gap-2">
             <input type="radio" name="question2" id="option3" />
-            <label htmlFor="option3">Option 3</label>
+            <label htmlFor="option3">
+              Kemampuan untuk mendeteksi kesalahan tipe pada waktu kompilasi
+            </label>
           </div>
           <div className="flex items-center gap-2">
             <input type="radio" name="question2" id="option4" />
-            <label htmlFor="option4">Option 4</label>
+            <label htmlFor="option4">Dukungan untuk arsitektur mikro</label>
           </div>
         </div>
       </div>
