6978cf2c1126005eac933aed891c64deba2d188e
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-12 23:03:33 +0700

    refactor: Update VideoCourse component to include 'to' prop for navigation

--- src/components/ui/VideoCourse.jsx
+++ src/components/ui/VideoCourse.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 import CardVideoCourse from "./CardVideoCourse";
 
-export default function VideoCourse({ image, title }) {
+export default function VideoCourse({ image, title, to }) {
   return (
     <div>
-      <CardVideoCourse image={image} title={title} />
+      <CardVideoCourse image={image} title={title} to={to} />
     </div>
   );
 }
